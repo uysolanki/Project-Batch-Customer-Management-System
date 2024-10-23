2a83c2bbd2d1ab1f4dbf53296514d4dda2d44081
Umar Solanki <uysolanki@gmail.com>
2024-10-23 20:10:12 +0530

    initial commit

---
+++ src/main/java/com/excelr/ProjectBatchCMS/ProjectBatchCmsApplication.java
@@ -0,0 +1,13 @@
+package com.excelr.ProjectBatchCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBatchCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBatchCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excelr/ProjectBatchCMS/ProjectBatchCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelr.ProjectBatchCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBatchCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
